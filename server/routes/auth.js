cbc04ba22d4ba60172472f6a954313c9667b8d40
Rishi Raj <106508188+Rishi1808@users.noreply.github.com>
2024-07-26 01:31:43 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -9,7 +9,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'https://notes-yard-full.onrender.com/auth/google/callback',
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     async function (accessToken, refreshToken, profile, done) {
       const newUser = {
